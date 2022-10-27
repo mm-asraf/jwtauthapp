a85b19cd20cad5c2604b6187b0d581c553aefd68
mahboob asraf <mahboobasraf@gmail.com>
2022-10-27 18:58:00 +0530

    latest codes

---
+++ authapp/src/test/java/com/authapp/authapp/AuthappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.authapp.authapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
